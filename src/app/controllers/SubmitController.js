71e932169c087eeeb6ce46419a43a487c0f238ec
phucnthe176442 <phucnthe176442@fpt.edu.vn>
2023-02-28 16:57:52 +0700

    compile using command

--- src/app/controllers/SubmitController.js
+++ src/app/controllers/SubmitController.js
@@ -1,72 +1,75 @@
-const Submission = require('../models/Submission');
-const User = require('../models/User');
-const fs = require('fs');
-const axios = require('axios');
-const qs = require('qs');
-const Testcase = require('../models/Testcase');
-const mongoose = require('mongoose');
+const Submission = require("../models/Submission");
+const User = require("../models/User");
+const Testcase = require("../models/Testcase");
+
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const fs = require("fs");
+const axios = require("axios");
+const qs = require("qs");
+
+const { exec } = require("child_process");
+
+const send = (testcase) =>
+  new Promise((resolve) => {
+    //const input = testcases[i].input; // input value to be passed to the C program
+    const cmd = __dirname + "\\solution";
+
+    const child = exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+
+    child.stdin.write(testcase.input);
+    child.stdin.end();
+    setTimeout(() => resolve(child.stdout), 5000);
+  });
+
+async function firstAsyncFunction() {
+  // compile the C code using GCC
+  const fileName = "solution.c";
+  const compileCommand =
+    `gcc -o ` + __dirname + `\\solution ` + __dirname + `\\${fileName}`;
+  exec(compileCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
+
+async function secondAsyncFunction(testcases) {
+  let correct = 0;
+  for (let i = 0; i < testcases.length; ++i) {
+    const output = await send(testcases[i]);
+    if (output === testcases[i].output) correct++;
+  }
+  return correct;
+}
+
+async function executeFunctions(testcases) {
+  return firstAsyncFunction().then(() => secondAsyncFunction(testcases));
+}
+
 class SubmitController {
   // [POST] /homepage/submit
-
   createSubmission(req, res, next) {
     if (req.session.user) {
-      let code = '#include<stdio.h> \nint main(){printf("Hello World");return 0;}';
-      code = fs.readFileSync('./src/public/solutions/solution.c');
-      Testcase.find({ task_name: req.body.task_name }).lean()
+      Testcase.find({ task_name: req.body.task_name })
+        .lean()
         .then((testcases) => {
-          var correct = 0;
-          for (let i =0; i< testcases.length;i++) {
-            
-            console.log(testcases[i].input);
-            let sendData = qs.stringify({
-              'code': code,
-              'language': 'c',
-              'input': testcases[i].input
-            });
-            //the config of axios to use CodeX API
-            let config = {
-              method: 'post',
-              url: 'https://api.codex.jaagrav.in',
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-              },
-              data: sendData
-            };
-            //send the code and get the result
-
-            axios(config)
-              .then(function (res2) {
-                console.log(res2.data.output);
-                if(res2.data.output===testcases[i].output)
-                  correct++;
-
-              })
-              .catch(function (err) {
-                console.log(err);
-              });
-          }
-          var status = 'Wrong answer';
-          if(correct==testcases.length){
-            
-           status='Accepted';
-            
-          }
-          var FormData ={
-            user_name: req.session.user,
-            task_name: req.body.task_name,
-            status: status,
-            slug: req.body.task_name
-          };
-          var submission = new Submission(FormData);
-          res.json(req.body);
-          submission.save();
-          // res.redirect("/homepage");
+          let cnt = executeFunctions(testcases);
+          res.json(cnt);
         })
         .catch((error) => next(error));
-
-    } 
-    else {
+    } else {
       res.redirect("/");
     }
   }
--- src/routes/submit.js
+++ src/routes/submit.js
@@ -2,18 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const SubmitController = require("../app/controllers/SubmitController");
-const multer = require('multer');
+const multer = require("multer");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
     //   cb(null, 'D:/Srping2023/JS/ProjectC/demo_project_entry_jsclub/src/public/solutions');
-    cb(null, './src/public/solutions');
+    cb(null, "./src/app/controllers");
   },
   filename: (req, file, cb) => {
-      cb(null, "solution.c");
-  }    
+    cb(null, "solution.c");
+  },
 });
-let upload = multer({storage: storage});
-router.use("/", upload.single('solution'), SubmitController.createSubmission);
+let upload = multer({ storage: storage });
+router.use("/", upload.single("solution"), SubmitController.createSubmission);
 
 module.exports = router;
