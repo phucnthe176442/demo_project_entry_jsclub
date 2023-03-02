bf9d96d7bb87ab6723871ab6da2aefa6141585c5
phucnthe176442 <phucnthe176442@fpt.edu.vn>
2023-03-02 07:01:04 +0700

    fix responsive

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -10,7 +10,7 @@ let storage = multer.diskStorage({
     cb(null, "./src/public/tasks");
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname+".pdf");
+    cb(null, "debai.pdf");
   },
 });
 let upload = multer({ storage: storage });
