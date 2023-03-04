2a7c9b760e60786094186349174f3ad73eea3a1b
DUC <tranminhducbxvp@gmail.com>
2023-03-04 17:44:02 +0700

    add user and password constraints

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -30,15 +30,21 @@ class UserController {
   // [POST] /homepage/users/create/
   create(req, res, next) {
     if (req.session.user) {
-      let FormData = {
-        user_name: req.body.user_name,
-        score: 0,
-        email: req.body.email,
-        pass_word: "1",
-      };
-      let user = new User(FormData);
-      user.save();
-      res.redirect("/homepage/users/showAll");
+      User.find({}).then((users) => {
+        if (users.length == 30)
+          res.send("Cannot add more user, try delete old users");
+        else {
+          let FormData = {
+            user_name: req.body.user_name,
+            score: 0,
+            email: req.body.email,
+            pass_word: "1",
+          };
+          let user = new User(FormData);
+          user.save();
+          res.redirect("/homepage/users/showAll");
+        }
+      })
     } else {
       res.redirect("/");
     }
@@ -58,12 +64,17 @@ class UserController {
   // [POST] /homepage/users/updateName
   updateName(req, res, next) {
     if (req.session.user) {
-      User.findOneAndUpdate({ user_name: req.body.old_username }, { user_name: req.body.new_username }).then((user) => {
-        Submission.updateMany({ user_name: req.body.old_username }, { user_name: req.body.new_username }).then((submissions) => {
-          req.session.user = req.body.new_username;
-          res.redirect("/homepage");
-        })
-      });
+      User.find({ user_name: req.body.new_username }).then((users) => {
+        if (users.length == 0)
+          User.findOneAndUpdate({ user_name: req.body.old_username }, { user_name: req.body.new_username }).then((user) => {
+            Submission.updateMany({ user_name: req.body.old_username }, { user_name: req.body.new_username }).then((submissions) => {
+              req.session.user = req.body.new_username;
+              res.redirect("/homepage");
+            })
+          });
+        else
+          res.send('Username cannot be duplicated')
+      })
     } else {
       res.redirect("/");
     }
@@ -72,11 +83,11 @@ class UserController {
   updatePass(req, res, next) {
     if (req.session.user) {
       User.findOneAndUpdate({ user_name: req.session.user, pass_word: req.body.old_password }, { pass_word: req.body.new_password }).then((user) => {
-        // if (user)
+        if (user)
           res.redirect("/homepage");
-        // else {
-          
-        // }
+        else {
+          res.send('Wrong old password');
+        }
       });
 
     } else {
